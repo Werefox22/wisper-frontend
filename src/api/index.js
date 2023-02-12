2188e588ee3ab3ce7466f23cd71b16894ac39abd
Werefox22 <jack@wrightus.com>
2023-02-12 17:33:59 -0500

    Exported the api url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 require('dotenv').config();
 
-const url = process.env.API_URL;
+export const url = process.env.API_URL;
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
