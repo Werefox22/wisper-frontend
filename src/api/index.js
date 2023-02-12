7a6a908a98ebb4ad1bb9c67aa9a11f15e6a6f681
Werefox22 <jack@wrightus.com>
2023-02-12 17:29:26 -0500

    Installed dotenv and updated the api url to use it

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = 'https://wisperapi.onrender.com/';
+require('dotenv').config();
+
+const url = process.env.API_URL;
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
